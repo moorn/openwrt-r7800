b036e5ec7b6b998d83ad17ba1d530867358ae06b
moorn <nomon@hotmail.com>
2020-04-07 10:27:04 +0800

    update
    update

--- diy.sh
+++ diy.sh
@@ -8,23 +8,6 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# 固件名称
-uci set system.@system[0].hostname=R7
-uci set system.@system[0].timezone=CST-8
-uci set system.@system[0].zonename=Asia/Shanghai
-uci commit system
-
-
-#设置默认管理IP地址
-uci set network.lan.ipaddr='10.0.0.1'
-uci set network.lan.netmask='255.255.255.0'
-uci set network.lan.delegate='0'
-uci set network.wan.delegate='0'
-uci commit network
-
-sed -i '/log-facility/d' /etc/dnsmasq.conf
-echo "log-facility=/dev/null" >> /etc/dnsmasq.conf
-
 # 添加第三方软件包
 git clone https://github.com/moorn/smartdns package/smartdns
 
